6e4405dba13da83845ebd63a272be781cc45da37
meagan <meagan.h.smith@gmail.com>
2021-07-23 11:01:34 -0500

    Attempt to display opportunity creator correctly

---
+++ frontend/src/store/user.js
@@ -0,0 +1,42 @@
+import { csrfFetch } from './csrf';
+
+const LOAD = 'user/getUser';
+
+const load = (userList) => {
+    return {
+        type: LOAD,
+        userList
+    }
+}
+
+export const getUsers = () => async dispatch => {
+    const res = await csrfFetch('/api/users');
+
+    if(res.ok) {
+        const usersList = await res.json();
+        dispatch(load(usersList));
+        return usersList;
+    }
+}
+
+const initialState = {};
+
+const userReducer = (state = initialState, action) => {
+    let newState = { ...state };
+
+    switch(action.type) {
+        case LOAD: {
+            const allUsers = {};
+
+            action.userList.forEach(user => {
+                allUsers[user.id] = user;
+            });
+
+            return { ...state, ...allUsers}
+        }
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
